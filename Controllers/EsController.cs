99690896a91ce97e76656d21dfcb7c1f41eef4fa
Mohamed Saleh <mohasaleh1991@gmail.com>
2023-11-25 22:47:31 +0200

    whatever

---
+++ Controllers/EsController.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ErpModV.Controllers
+{
+    public class EsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult BidStatus()
+        {
+            return View();
+        }
+
+        public ActionResult NewBid()
+        {
+            return View();
+        }
+
+        public ActionResult BidPricing()
+        {
+            return View();
+        }
+
+        public ActionResult PriceOffer()
+        {
+            return View();
+        }
+
+        public ActionResult Contracts()
+        {
+            return View();
+        }
+
+        public ActionResult ExecutiveSummary()
+        {
+            return View();
+        }
+
+        public ActionResult ClientRec()
+        {
+            return View();
+        }
+
+
+        public ActionResult ContractorAgreement()
+        {
+            return View();
+        }
+
+        public ActionResult ContractorInvoices()
+        {
+            return View();
+        }
+
+        public ActionResult Labor()
+        {
+            return View();
+        }
+
+        public ActionResult Materials()
+        {
+            return View();
+        }
+
+        public ActionResult CashExpenses()
+        {
+            return View();
+        }
+
+        public ActionResult ExecutionSummaries()
+        {
+            return View();
+        }
+
+        public ActionResult CostReports()
+        {
+            return View();
+        }
+
+        public ActionResult DailyReports()
+        {
+            return View();
+        }
+
+        public ActionResult DeductionAndAdditionReports()
+        {
+            return View();
+        }
+
+        public ActionResult FinancialPositions()
+        {
+            return View();
+        }
+
+        public ActionResult Clients()
+        {
+            return View();
+        }
+
+        public ActionResult ManagersAndConsultants()
+        {
+            return View();
+        }
+
+        public ActionResult Subcontractors()
+        {
+            return View();
+        }
+
+        public ActionResult ProjectStatus()
+        {
+            return View();
+        }
+
+        public ActionResult PricingStatus()
+        {
+            return View();
+        }
+
+        public ActionResult ReviewStatus()
+        {
+            return View();
+        }
+
+        public ActionResult ContractStatus()
+        {
+            return View();
+        }
+
+        public ActionResult ContractModels()
+        {
+            return View();
+        }
+
+        public ActionResult ContractTypes()
+        {
+            return View();
+        }
+
+        public ActionResult ItemModels()
+        {
+            return View();
+        }
+
+        public ActionResult EntryModels()
+        {
+            return View();
+        }
+
+        public ActionResult EntryTypes()
+        {
+            return View();
+        }
+
+        public ActionResult DailyStatementModels()
+        {
+            return View();
+        }
+
+        public ActionResult DocModels()
+        {
+            return View();
+        }
+
+        public ActionResult DocTreas()
+        {
+            return View();
+        }
+
+        public ActionResult PaymentReceiptTypes()
+        {
+            return View();
+        }
+
+        public ActionResult PaymentMovementTypes()
+        {
+            return View();
+        }
+
+        public ActionResult InvoiceModels()
+        {
+            return View();
+        }
+
+        public ActionResult InvoiceTypes()
+        {
+            return View();
+        }
+
+        public ActionResult InvoiceBook()
+        {
+            return View();
+        }
+
+        public ActionResult Suppliers()
+        {
+            return View();
+        }
+
+        public ActionResult ItemCategories()
+        {
+            return View();
+        }
+
+        public ActionResult ItemTypes()
+        {
+            return View();
+        }
+    }
+}
